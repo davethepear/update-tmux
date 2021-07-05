5c24efb86340e7123451ce7dea4b052755db21b6
Dave the Pear <86448428+davethepear@users.noreply.github.com>
2021-07-05 07:05:12 -0500

    Update update.sh
    I had it backward... and forgot the variable! wtaf?

--- update.sh
+++ update.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-if [ ! -z "$muxt" ]; then
+muxt=`tmux ls  2>/dev/null | awk '{ print substr($1, 1, length($1)-1) }'`
+if [ -z "$muxt" ]; then
 tmux new -A -s updating \; \
   send-keys 'sudo apt update && sudo apt dist-upgrade -y' C-m \;
 fi
 # or use aptitude  send-keys 'sudo aptitude update && sudo aptitude safe-upgrade -y' C-m \;
-if [ -z "$muxt" ]; then
+if [ ! -z "$muxt" ]; then
   sudo apt update && sudo apt upgrade -y
 fi
