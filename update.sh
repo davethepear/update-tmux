14efcfa6b1203a08d0c5ca50b38710081eb2b58e
Dave the Pear <86448428+davethepear@users.noreply.github.com>
2025-10-20 19:31:15 -0500

    Update update.sh
    eh, it works

--- update.sh
+++ update.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-muxt=`tmux ls  2>/dev/null | awk '{ print substr($1, 1, length($1)-1) }'`
-if [ -z "$muxt" ]; then
-tmux new -A -s updating \; \
-  send-keys 'sudo apt update && sudo apt dist-upgrade -y' C-m \;
-fi
-# or use aptitude  send-keys 'sudo aptitude update && sudo aptitude safe-upgrade -y' C-m \;
-if [ ! -z "$muxt" ]; then
-  sudo apt update && sudo apt upgrade -y
-fi
+
+LOGFILE="$HOME/update-$(date +%F-%H%M%S).log"
+[[ -z "$TMUX" ]] && exec tmux new -s sysupdate "bash '$0'"
+apt list --upgradable | tee -a "$LOGFILE"
+echo -n "Proceed with upgrade? [y/N] "; read -r yn
+[[ "$yn" =~ ^[Yy]$ ]] && sudo apt update && sudo apt dist-upgrade -y && sudo flatpak update -y | tee -a "$LOGFILE"
+echo "Done."
